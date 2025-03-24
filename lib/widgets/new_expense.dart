671d88e54d56394d7ba8845538eb8726639b6334
Satyam Kumar <Satyam.Kumar@winjit.com>
2025-03-24 11:47:04 +0530

    added cancel and save button in bottom sheet

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,26 +10,59 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   // _NewExpenseState({super.key});
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(20.0),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             decoration: InputDecoration(labelText: 'Title'),
             maxLength: 50,
           ),
-          const SizedBox(height: 5),
-          Expanded(
-            child: Row(
-              children: [
-                TextField(
-                  decoration: InputDecoration(labelText: 'Amount'),
-                  //keyboardType: TextInputType.number,
+          //const SizedBox(height: 5),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  decoration: InputDecoration(
+                    labelText: 'Amount',
+                    prefixText: '\$ ',
+                  ),
+                  keyboardType: TextInputType.number,
                 ),
-              ],
-            ),
+              ),
+            ],
+          ),
+
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: Text('Save Expenses'),
+              ),
+            ],
           ),
         ],
       ),
